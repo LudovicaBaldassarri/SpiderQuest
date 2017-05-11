2093f8386922129025c5f99635313d35fe5b7b6f
AleAgo <alessia.agostinis@gmail.com>
2017-05-11 18:12:19 +0200

    Pagina di vittoria e sconfitta con gif e tasti per tornare a giocare o al menù.

--- app/src/main/java/com/example/alessia/appragni/LooseActivity.java
+++ app/src/main/java/com/example/alessia/appragni/LooseActivity.java
@@ -1,7 +1,9 @@
 package com.example.alessia.appragni;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by Michele on 03/05/2017.
@@ -11,5 +13,15 @@ public class LooseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loose);
+    }
+
+    public void backtoMenu (View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void openInizio(View view){
+        Intent intent=new Intent(this, Game1Activity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/alessia/appragni/VictoryActivity.java
+++ app/src/main/java/com/example/alessia/appragni/VictoryActivity.java
@@ -1,7 +1,9 @@
 package com.example.alessia.appragni;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by ludo_monkey on 03/05/17.
@@ -13,5 +15,15 @@ public class VictoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_victory);
+    }
+
+    public void backtoMenu (View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void openInizio(View view){
+        Intent intent=new Intent(this, Game1Activity.class);
+        startActivity(intent);
     }
 }
